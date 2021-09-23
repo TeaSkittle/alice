c4121f5bd5d3a6d5da62817973392ba116c209ac
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2021-09-23 14:54:56 -0600

    Update fileh.rkt

--- fileh.rkt
+++ fileh.rkt
@@ -3,6 +3,7 @@
 ;; Date: 7-4-2021
 (require racket/file)
 (require racket/list)
+(provide (all-defined-out)) ; Let all funcitons in file be used in other files
 
 #|
   TODO:
@@ -57,23 +58,3 @@
 ;; Remove specific line from file
 (define (remove-line file num)
   (remove-lines file num 1))
-
-;;
-;; Stuff most likely to be moved to another rkt file
-;;
-;; Todo file location
-(define task-file "/tmp/tasks.txt")
-
-;; Add a task
-(define (task-add task)
-  (append-file task-file
-               (string-append task " \n")))
-
-;; Delete a task
-(define (task-delete task-number)
-  (remove-line task-file task-number))
-
-;; List tasks
-(define (task-list)
-  (line-print task-file))
-
