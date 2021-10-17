89b36a5ef45398ffab0169abc12c455fd1e3fb45
Travis Dowd <teaskittle@gmail.com>
2021-10-17 11:31:18 -0600

    add linux.rkt

---
+++ linux.rkt
@@ -0,0 +1,13 @@
+;; Date: 10-17-2021
+;; Functions for Linux commands
+#lang racket/base
+
+(require racket/system)
+(require "web.rkt")
+(provide (all-defined-out))
+
+;; Scan directory with ClamAV, ClamAV's a dependancy
+(define (linux-scan directory)
+  (system "sudo freshclam")
+  (system (string-append "sudo clamscan -i -r --bell " directory)))
+
--- main.rkt
+++ main.rkt
@@ -2,10 +2,13 @@
 #lang racket/base
 
 (require racket/cmdline)
-(require "todo.rkt")
+(require "task.rkt")
 (require "web.rkt")
+(require "linux.rkt")
 
 ;; cmd-line guide: https://jackwarren.info/posts/guides/racket/racket-command-line/?utm_source=pocket_mylist
+
+(define version "Testing")
 
 (define parser
   (command-line
@@ -13,6 +16,8 @@
    "A simple digital personal assistant written in Racket."
    
    #:once-each
+   [("--version") "Display Alice version"
+                  (printf "Alice: ~a\n" version)]
    [("-l" "--list-tasks") "List tasks in todo list"
                           (task-list)]
    [("-a" "--add-task") TASK
@@ -30,6 +35,9 @@
    [("-g" "--get-file") FILE
                         "Download file from the web"
                         (web-get FILE)]
+   [("-c" "--clamav-scan") DIR
+                              "Scan for infected files in specified directory with ClamAV"
+                              (linux-scan DIR)]
    
    #:args () (void)))
 
