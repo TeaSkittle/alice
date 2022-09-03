8ed8a9bbc45953092c20c6e3bb9760e4a2dee653
Travis Dowd <59617683+TeaSkittle@users.noreply.github.com>
2022-09-03 07:55:01 -0600

    Update make.sh

--- make.sh
+++ make.sh
@@ -7,9 +7,9 @@ echo "Installing dependencies..."
 # Will be checking for installs rather than installing outright
 # Tell user to install dependancies before install alice
 #
-sudo apt-get install pandoc clamav gzip python3 racket -y
+#sudo apt-get install pandoc clamav gzip python3 racket -y
 echo "Generating man page..."
-pandoc /docs/alice.md -s -t man -o /docs/alice.1
+pandoc docs/alice.md -s -t man -o docs/alice.1
 if [ ! -d "/usr/local/man/man1" ]; then
     sudo mkdir /usr/local/man/man1
 fi
@@ -18,11 +18,11 @@ if [ ! -d "~/.config/alice" ]; then
     mkdir ~/.config/alice
 fi
 touch ~/.config/alice/todo.txt
-sudo mv /docs/alice.1 /usr/local/man/man1
+sudo mv docs/alice.1 /usr/local/man/man1
 sudo gzip /usr/local/man/man1/alice.1
 sudo mandb
 echo "Making alice executable..."
-raco exe -o alice /src/main.rkt
+raco exe -o alice src/main.rkt
 echo "Moving to /usr/local/bin/alice..."
 sudo mv alice /usr/local/bin/
 echo "alice setup compelete\n"
